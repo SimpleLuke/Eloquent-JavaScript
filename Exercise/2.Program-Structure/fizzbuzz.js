8e489cc4230d1508047c27f3525d42e400a21340
Luke Lai <lukelai.dev@gmail.com>
2023-05-26 23:52:28 +0100

    Update my answer on FizzBuzz and my take on suggested solution

---
+++ Exercise/2.Program-Structure/fizzbuzz.js
@@ -0,0 +1,27 @@
+function fizzbuzz() {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+fizzbuzz();
+
+// Here is the suggested solution below:
+// I agree it is a more readable code. With less condition statement, the flow can be tracked easier.
+
+for (let i = 1; i <= 100; i++) {
+  let output = "";
+
+  if (i % 3 === 0) output += "Fizz";
+  if (i % 5 === 0) output += "Buzz";
+
+  console.log(output || i);
+}
